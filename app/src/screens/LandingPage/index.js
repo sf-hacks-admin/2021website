45fa10ef02f6c1a690cd044b24b8c1b4885959af
xo28122000 <jainam.2000@yahoo.com>
2020-08-17 14:39:38 -0400

    init

---
+++ app/src/screens/LandingPage/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Header from "./Header";
+import Footer from "./Footer";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Header />
+      <Footer />
+    </div>
+  );
+};
+
+export default LandingPage;
