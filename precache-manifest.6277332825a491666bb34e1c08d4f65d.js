dabb32327091291d0ead5c4302958299624a3747
xo28122000 <jainam.2000@yahoo.com>
2020-08-18 16:28:25 -0400

    deploy 4

---
+++ precache-manifest.6277332825a491666bb34e1c08d4f65d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89efe7406c3a995781d21fbf5a7f849a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0e9dbf9546d49062c8ab",
+    "url": "./static/css/main.935a406d.chunk.css"
+  },
+  {
+    "revision": "eb12240d8ab4ae22be52",
+    "url": "./static/js/2.bea9d31d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.bea9d31d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e9dbf9546d49062c8ab",
+    "url": "./static/js/main.07058a74.chunk.js"
+  },
+  {
+    "revision": "e86c2ff2f1eb78554e57",
+    "url": "./static/js/runtime-main.0660989e.js"
+  },
+  {
+    "revision": "b51d33f3a7de7820da99a4fd19c591a1",
+    "url": "./static/media/Background.b51d33f3.svg"
+  },
+  {
+    "revision": "8d25b10dc8f1baa75aeffb2c00853f1b",
+    "url": "./static/media/Facebook_Icon.8d25b10d.svg"
+  },
+  {
+    "revision": "495030d33ef4f49440a5bc2eb9297b9c",
+    "url": "./static/media/Instagram_Icon.495030d3.svg"
+  },
+  {
+    "revision": "96be44eef6903ec39d2ba7668cc4c3ac",
+    "url": "./static/media/SF_Hacks_Logo.96be44ee.svg"
+  },
+  {
+    "revision": "f5a055619d9cb181a900824d3d4a8b5f",
+    "url": "./static/media/Twitter_Icon.f5a05561.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.db5794f503213cb63d70d7f720c8010f.js"
+  "./precache-manifest.6277332825a491666bb34e1c08d4f65d.js"
 );
 
 self.addEventListener('message', (event) => {
